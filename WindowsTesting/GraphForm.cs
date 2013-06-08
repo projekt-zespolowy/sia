4652a72573af59edb00fb9c277f55c19e9870cc5
Projekt Zespolowy <alef@wozniak.com>
2013-06-09 00:27:31 +0200

    Added lines and labels to graph

--- WindowsTesting/GraphForm.cs
+++ WindowsTesting/GraphForm.cs
@@ -14,6 +14,7 @@ namespace WindowsTesting
     {
         Hub hub;
         List<EventInfo> lei;
+        EventInfo active_event;
         Queue<float> values = new Queue<float>();
         private Timer timer; 
 
@@ -26,7 +27,7 @@ namespace WindowsTesting
         {
             InitializeComponent();
             hub = h;
-            max_value_num = 100;
+            max_value_num = 50;
             dot_size = 4;
             lei = new List<EventInfo>();
 
@@ -66,8 +67,9 @@ namespace WindowsTesting
         }
 
         private void timer_Tick(object sender, EventArgs e)
-        {
-            float val = (float)lei[EID].kursy[CID];
+        {           
+            active_event = hub.GetEventInfo(EID);
+            float val = (float)active_event.kursy[CID];
 
             PushValue(val);
             this.GraphBox.Refresh();
@@ -80,29 +82,52 @@ namespace WindowsTesting
 
         private void GraphBox_Paint(object sender, PaintEventArgs e)
         {
-            int width = this.GraphBox.Width-20;
+            int width = this.GraphBox.Width-50;
             int height = this.GraphBox.Height-30;
-            int x, y;
+            int x, y, x_prev, y_prev;
             float val;
             float spread = (float)width / (float)max_value_num;
 
             Brush brush_black = new SolidBrush(Color.Black);
             Brush brush_green = new SolidBrush(Color.Green);
-            Pen pen = new Pen(Color.Green, 2.0F);
+            Pen pen2 = new Pen(Color.Green, 2.0F);
+            Pen pen1 = new Pen(Color.Green, 1.0F);
+            Font drawFont = new Font("Arial", 8);
 
             e.Graphics.FillRectangle(brush_black, 10, 20, width, height);
-            e.Graphics.DrawRectangle(pen, 10, 20, width, height);
+            e.Graphics.DrawRectangle(pen2, 10, 20, width, height);
 
-            for (int i = 0; i < values.Count(); i++)
+            x = width + 15;
+            y = 10 + height - (height * 9 / 10);
+            e.Graphics.DrawString(maximum_value.ToString(), drawFont, brush_green, x, y);
+
+            x = width + 15;
+            y = 10 + height;
+            e.Graphics.DrawString(0.ToString(), drawFont, brush_green, x, y);
+
+            if (values.Count() > 0)
             {
-                val = values.ElementAt(i) / maximum_value * ((float)height * 3 / 4);
-                x = 10 + width - i * (int)spread - dot_size;
+                val = values.ElementAt(0) / maximum_value * ((float)height * 9 / 10);
+                x = 10 + width - dot_size;
                 y = 20 + height - (int)val - dot_size;
+                for (int i = 1; i < values.Count(); i++)
+                {
+                    x_prev = x;
+                    y_prev = y;
 
-                e.Graphics.FillEllipse(brush_green, x - dot_size / 2, y - dot_size / 2, dot_size, dot_size);
+                    val = values.ElementAt(i) / maximum_value * ((float)height * 9 / 10);
+                    x = 10 + width - i * (int)spread;
+                    y = 20 + height - (int)val;
+
+                    //e.Graphics.FillEllipse(brush_green, x - dot_size / 2, y - dot_size / 2, dot_size, dot_size);
+
+                    e.Graphics.DrawLine(pen1, x_prev, y_prev, x, y);
+
+                }
             }
 
-            pen.Dispose();
+            pen1.Dispose();
+            pen2.Dispose();
             brush_black.Dispose();
             brush_green.Dispose();
         }
@@ -115,14 +140,14 @@ namespace WindowsTesting
                 comboBox2.Items.Add(str);
             }
             comboBox2.SelectedIndex = 0;
-            EID = comboBox1.SelectedIndex;
+            EID = lei[comboBox1.SelectedIndex].eid;
             CID = comboBox2.SelectedIndex;
             values.Clear();
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            EID = comboBox1.SelectedIndex;
+            EID = lei[comboBox1.SelectedIndex].eid;
             CID = comboBox2.SelectedIndex;
             values.Clear();
         }
